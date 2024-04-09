869519e7989be27c373539afdc30f98a66e3d10c
Jose Von Chong <javch1995@gmail.com>
2024-04-09 16:55:23 -0500

    init for display of instructions

---
+++ src/routes/+page.svelte
@@ -0,0 +1,75 @@
+
+
+<div id="couponBanner" class="!py-8 !bg-backgound-lightblue">
+    <div class="!container !mx-auto">
+       <div class="!flex !items-center !justify-center !gap-16">
+            <img class="!w-48 " src="https://www.copaair.com/promotions/destinations/icons/de-instrucc-2.png" alt="Desde este correo, dale clic al botón “Compra ya” que te llevará a nuestro sitio donde podrás utilizar el cupón de descuento." />
+     
+         <h2 class=" !text-h2 !font-heading-medium !font-heading  !text-primary  ">
+            ¡Tu cupón ha sido aplicado, disfruta de este
+            <span class="!font-heading !text-secondary !italic ">descuento</span> para ti!
+        </h2>
+       </div>
+    </div>
+</div>
+asdasd
+
+<div id="couponBanner" class=" !py-16 !bg-backgound-lightblue ">
+	<h3 class="!text-h3 !font-heading !text-primary !font-heading-bold !text-center !py-4">
+        ¿Cómo acceder a este descuento?
+	</h3>
+	<div
+		class="!container !mx-auto grid !grid-cols-1 md:!grid-cols-2 lg:!grid-cols-4 !auto-rows-auto !gap-24 m-auto">
+		<div class="text-center  font-heading">
+			<img class="!w-96 !mx-auto !mb-16" src="https://www.copaair.com/promotions/destinations/icons/de-instrucc-1.png" alt="Desde este correo, dale clic al botón “Compra ya” que te llevará a nuestro sitio donde podrás utilizar el cupón de descuento." />
+			<h3 class="!text-h3 !mb-8 !text-primary font-heading-bold">1°</h3>
+			<p>Desde este correo, dale clic al botón “Compra ya” que te llevará a nuestro sitio donde podrás utilizar el cupón de descuento.</p>
+		</div>
+
+		<div class="text-center font-heading">
+			<img class="!w-96 !mx-auto !mb-16" src="https://www.copaair.com/promotions/destinations/icons/de-instrucc-2.png" alt="Desde este correo, dale clic al botón “Compra ya” que te llevará a nuestro sitio donde podrás utilizar el cupón de descuento." />
+			<h3 class="!text-h3 !mb-8 !text-primary font-heading-bold">2°</h3>
+			<p>Desde este correo, dale clic al botón “Compra ya” que te llevará a nuestro sitio donde podrás utilizar el cupón de descuento.</p>
+		</div>
+		<div class="text-center font-heading">
+			<img class="!w-96 !mx-auto !mb-16" src="https://www.copaair.com/promotions/destinations/icons/de-instrucc-3.png" alt="Selecciona las fechas de viaje entre el 8 de febrero y el 30 de junio del 2023 y la ruta de tu preferencia dentro del listado de rutas disponibles." />
+			<h3 class="!text-h3 !mb-8 !text-primary font-heading-bold">3°</h3>
+			<p>Selecciona las fechas de viaje entre el 8 de febrero y el 30 de junio del 2023 y la ruta de tu preferencia dentro del listado de rutas disponibles.</p>
+		</div>
+		<div class="text-center font-heading">
+			<img class="!w-96 !mx-auto !mb-16" src="https://www.copaair.com/promotions/destinations/icons/de-instrucc-4.png" alt="Presiona “Buscar” y tendrás hasta un 15% de descuento sobre la tarifa base final (sin combustible ni impuestos) al final de la compra." />
+			<h3 class="!text-h3 !mb-8 !text-primary font-heading-bold">4°</h3>
+			<p>Presiona “Buscar” y tendrás hasta un 15% de descuento sobre la tarifa base final (sin combustible ni impuestos) al final de la compra.</p>
+		</div>
+	</div>
+
+</div>
+
+<!--
+
+    <script type="text/javascript">
+    (function deleteAll() {
+        const x = document.querySelectorAll(".__pfs, .__bss");
+        for (i = 0; i < x.length; i++) {
+            x[i].classList.remove('__pfs', '__bss');
+        }
+    })();
+</script>
+
+<script>
+var urlParams = new URLSearchParams(window.location.search);
+var couponParam = urlParams.get('em_dc');
+var pattern = /[A-Z0-9]{7}/
+var esValid = pattern.test(couponParam);
+
+if (esValid = true) {  
+    document.getElementById('couponBanner').classList.remove('hidden')
+}
+
+</script>
+
+
+
+
+
+-->
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const cmpreset = require('cmds-tailwind-styles');
+
+module.exports = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/cmds-component-*/*.{html,js,svelte,ts}'
+	],
+	presets: [cmpreset]
+};
